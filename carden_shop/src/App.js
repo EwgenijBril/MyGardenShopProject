8c8f5c8f71e5f9412c2ac3d0238ca524883f222b
Evgeny <bril424@mail.ru>
2023-01-27 08:26:43 +0100

    routing and nav_menu added

--- carden_shop/src/App.js
+++ carden_shop/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CategoriesPage from './pages/CategoriesPage'
+import ProductsPage from './pages/ProductsPage'
+import ProductDescrPage from './pages/ProductDescrPage'
+import CartPage from './pages/CartPage'
+import Layout from './components/Layout';
+
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<HomePage/>} />
+          <Route path='/categories' element={<CategoriesPage/>} />
+          <Route path='/category' element={<ProductsPage/>} />
+          <Route path='/product/:id' element={<ProductDescrPage/>} />
+          <Route path='/cart' element={<CartPage/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- carden_shop/src/components/Nav/index.jsx
+++ carden_shop/src/components/Nav/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import logo from './media/image 1.png'
+import { Link } from 'react-router-dom'
+import { ShoppingOutlined } from '@ant-design/icons'
+import s from './index.module.css'
 
 export default function Nav() {
+
   return (
-    <div>Nav</div>
+    <nav className={s.nav}>
+      <header className={s.header}>
+        <img src={logo} alt='logo' />
+        <button>Catalog</button>
+      </header>
+
+      <div className={s.nav_menu}>
+        <Link to='/'>categories</Link>
+        <Link>coupon</Link>
+        <Link>promotions</Link>
+        <Link>constacts</Link>
+        <ShoppingOutlined className={s.cart}/>
+      </div>
+    </nav>
   )
 }
