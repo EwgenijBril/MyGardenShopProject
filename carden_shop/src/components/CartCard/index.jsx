c8e1d78ad53ae6b508b2c39fafe4f1b896abe533
Evgeny <bril424@mail.ru>
2023-02-14 11:30:48 +0100

    incrementCount and decrementCount added

--- carden_shop/src/components/CartCard/index.jsx
+++ carden_shop/src/components/CartCard/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import s from './index.module.css'
 import { RxCross2 } from 'react-icons/rx'
+import { useDispatch } from 'react-redux'
+import { incrementCount, decrementCount } from '../../store/reducers/cart'
 
 export default function CartCard({ id, title, image, discont_price, price, count }) {
+
+    const dispatch = useDispatch();
+
+    const increment_count = () => dispatch(incrementCount(id))
+    const decrement_count = () => dispatch(decrementCount(id))
+
   return (
     <div className={s.cart_card}>
         <div className={s.left_block}>
@@ -10,9 +18,9 @@ export default function CartCard({ id, title, image, discont_price, price, count
             <div className={s.info_block}>
                 <p className={s.title}>{title}</p>
                 <div className={s.card_button}>
-                    <button>-</button>
-                    <p>1</p>
-                    <button>+</button>
+                    <button onClick={decrement_count}>-</button>
+                    <p>{ count }</p>
+                    <button onClick={increment_count}>+</button>
                 </div> 
             </div>  
         </div>
--- carden_shop/src/store/reducers/cart.js
+++ carden_shop/src/store/reducers/cart.js
@@ -1,8 +1,12 @@
 const defaultState = [];
 
 const ADD_TO_CART = 'ADD_TO_CART';
+const INCREMENT_COUNT = 'INCREMENT_COUNT'
+const DECREMENT_COUNT = 'DECREMENT_COUNT'
 
 export const addToCard = payload => ({type: ADD_TO_CART, payload});
+export const incrementCount = payload => ({type: INCREMENT_COUNT, payload});
+export const decrementCount = payload => ({type: DECREMENT_COUNT, payload});
 
 const checkProduct = ( state, payload ) => {
     const productInState = state.find( el => el.id === payload.id );
@@ -20,8 +24,19 @@ const checkProduct = ( state, payload ) => {
 export const cartReducer = ( state = defaultState, action ) => {
     if ( action.type === ADD_TO_CART ) {
         return checkProduct( state, action.payload )   
+    } else if ( action.type === INCREMENT_COUNT ) {
+        state.find( el => el.id === action.payload ).count++
+        return [...state] 
+    } else if (action.type === DECREMENT_COUNT) {
+        const target_card = state.find( el => el.id === action.payload )
+
+        target_card.count === 1
+        ? state = state.filter( el => el.id !== action.payload )
+        : target_card.count--
+        
+        return [...state]
     } else {
         return state
     }
 }
-
+ 
