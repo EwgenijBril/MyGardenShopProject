c137e7c5d0baa3b417392476dc7e739d851d6cf4
Evgeny <bril424@mail.ru>
2023-02-18 15:06:52 +0100

    scroll added

--- carden_shop/src/components/DiscountCoupon/index.jsx
+++ carden_shop/src/components/DiscountCoupon/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import discountImg from './media/image 3.png'
-import s from './index.module.css'
+import React from "react";
+import discountImg from "./media/image 3.png";
+import s from "./index.module.css";
 
 export default function DiscountCoupon() {
   return (
-    <section className={s.section_coupon}>
-      <img src={ discountImg } alt="discountImg" />
-        <form className={s.container_form}>
-          <div>
-              <h2>Sale 5%</h2>
-              <h3>for first ordner</h3>
-          </div>
-          <input type="number" placeholder='+49' />
-          <button>Get coupon</button>
-          </form>
+    <section className={s.section_coupon} id="coupon">
+      <img src={discountImg} alt="discountImg" />
+      <form className={s.container_form}>
+        <div>
+          <h2>Sale 5%</h2>
+          <h3>for first ordner</h3>
+        </div>
+        <input type="number" placeholder="+49" />
+        <button>Get coupon</button>
+      </form>
     </section>
-  )
+  );
 }
--- carden_shop/src/components/Nav/index.jsx
+++ carden_shop/src/components/Nav/index.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import logo from './media/image 1.png'
-import { Link } from 'react-router-dom'
-import { ShoppingOutlined } from '@ant-design/icons'
-import s from './index.module.css'
+import React from "react";
+import logo from "./media/image 1.png";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import { ShoppingOutlined } from "@ant-design/icons";
+import s from "./index.module.css";
 
 export default function Nav() {
-
   return (
     <section className={s.nav_section}>
       <nav className={s.nav}>
         <header className={s.header}>
-          <Link to='/'><img src={logo} alt='logo' /></Link>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
           <button>Catalog</button>
         </header>
 
         <div className={s.nav_menu}>
-          <Link to='categories'>Categories</Link>
-          <Link>Coupon</Link>
-          <Link to='products'>Sale</Link>
+          <Link to="categories">Categories</Link>
+          <HashLink smooth to="#coupon">
+            Coupon
+          </HashLink>
+          <Link to="products">Sale</Link>
           <Link>Constacts</Link>
-          <Link to='cart'>
+          <Link to="cart">
             <ShoppingOutlined className={s.cart} />
           </Link>
-          
         </div>
       </nav>
     </section>
-  )
+  );
 }
