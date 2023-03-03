35e0f2dd7063dbc3cb3bb468a35b3f5c8122b766
Evgeny <bril424@mail.ru>
2023-03-03 10:35:13 +0100

    img SalesBanner added

--- carden_shop/src/components/SalesBanner/index.jsx
+++ carden_shop/src/components/SalesBanner/index.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import saleImg from './media/image 2.png'
-import s from './index.module.css'
+import React from "react";
+import saleImg from "./media/image 21.png";
+import s from "./index.module.css";
 
 export default function SalesBanner() {
   return (
     <section className={s.section}>
-        <div className={s.section_container}>
-           <div className={s.left_block}>
-                <h1>Sale-Finale:</h1>
-                <h2>Up to 70% discont</h2>  
-                <button className={s.left_btn}>All promotions</button>
-                <button className={s.right_btn}>More...</button>
-            </div>
-       
-            <div className={s.right_block}>
-                <img src={saleImg} alt="saleImg" />
-            </div> 
+      <div className={s.section_container}>
+        <div className={s.left_block}>
+          <h1>Sale-Finale:</h1>
+          <h2>Up to 70% discont</h2>
+          <button className={s.left_btn}>All promotions</button>
+          <button className={s.right_btn}>More...</button>
         </div>
+
+        <div className={s.right_block}>
+          <img src={saleImg} alt="saleImg" />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
