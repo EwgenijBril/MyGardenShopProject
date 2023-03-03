607796c200a75dd2a7056d6c2d502090932be5d3
Evgeny <bril424@mail.ru>
2023-03-03 10:05:55 +0100

    request's product_item updated

--- carden_shop/src/requests/product_item.js
+++ carden_shop/src/requests/product_item.js
@@ -4,10 +4,6 @@ export const load_product = id => {
    return dispatch => {
         fetch(`http://localhost:3333/products/${id}`)
         .then(resp => resp.json())
-        .then(json => {
-            const [product_data] = json;
-            const { title, description, price, image, discont_price } = product_data
-        dispatch( loadProduct({title, image, description, price, discont_price}))
-    })
-    }  
+        .then(json => dispatch(loadProduct(json[0])))
+    }
 }
